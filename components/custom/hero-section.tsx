25af86c95c206a7fb2a8953a1a6716d2a8eb7716
Rodrigo-Vildosola <rjvildosola@miuandes.cl>
2024-07-15 01:57:40 -0400

    Hero Section!

--- components/custom/hero-section.tsx
+++ components/custom/hero-section.tsx
@@ -15,8 +15,8 @@ const HeroSection: React.FC<HeroSectionProps> = ({ font }) => {
   const t = useTranslations('HomePage');
 
   const getRandomSpeed = () => {
-    const min = 9;
-    const max = 12;
+    const min = 20;
+    const max = 25;
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
@@ -25,38 +25,62 @@ const HeroSection: React.FC<HeroSectionProps> = ({ font }) => {
       <div className="flex flex-col h-full">
         {/* Top Section */}
         <div className="flex h-48 md:h-64 rounded-md px-10 md:px-20 py-5 md:py-10 justify-center items-center relative ">
-          <div className={cn("h-full w-full text-gray-900 dark:text-gray-100 bg-gradient-to-r flex-col overflow-hidden from-sky-500 to-blue-800 dark:from-blue-700 dark:to-gray-900 rounded-md flex justify-center items-center", font.className)}>
+          <div className={cn("h-full w-full text-gray-800 dark:text-gray-100 sm:bg-gradient-radial md:bg-gradient-to-r flex-col overflow-hidden from-sky-500 to-blue-800 dark:from-blue-700 dark:to-gray-900 rounded-md flex justify-center items-center", font.className)}>
             <div className={cn(styles['move-left-to-right'], "text-xl whitespace-nowrap ")} style={{ animationDuration: `${getRandomSpeed()}s` }}>
-              <span className="mx-6">Unit Testing</span>
-              <span className="mx-6">RESTful API Development</span>
-              <span className="mx-6">AWS</span>
-              <span className="mx-6">Docker</span>
-              <span className="mx-6">Git</span>
+              <span className="mx-6 md:mx-10">Unit Testing</span>
+              <span className="mx-6 md:mx-10">RESTful API Development</span>
+              <span className="mx-6 md:mx-10">AWS</span>
+              <span className="mx-6 md:mx-10">Docker</span>
+              <span className="mx-6 md:mx-10">Git</span>
+              <span className="mx-6 md:mx-10">Unit Testing</span>
+              <span className="mx-6 md:mx-10">RESTful API Development</span>
+              <span className="mx-6 md:mx-10">AWS</span>
+              <span className="mx-6 md:mx-10">Docker</span>
+              <span className="mx-6 md:mx-10">Git</span>
             </div>
             <div className={cn(styles['move-right-to-left'], "text-xl whitespace-nowrap ")} style={{ animationDuration: `${getRandomSpeed()}s` }}>
-              <span className="mx-6">PostgreSQL</span>
-              <span className="mx-6">MongoDB</span>
-              <span className="mx-6">MySQL</span>
-              <span className="mx-6">NumPy</span>
-              <span className="mx-6">Pandas</span>
-              <span className="mx-6">TensorFlow</span>
-              <span className="mx-6">Keras</span>
+              <span className="mx-6 md:mx-10">PostgreSQL</span>
+              <span className="mx-6 md:mx-10">MongoDB</span>
+              <span className="mx-6 md:mx-10">MySQL</span>
+              <span className="mx-6 md:mx-10">NumPy</span>
+              <span className="mx-6 md:mx-10">Pandas</span>
+              <span className="mx-6 md:mx-10">TensorFlow</span>
+              <span className="mx-6 md:mx-10">Keras</span>
+              <span className="mx-6 md:mx-10">PostgreSQL</span>
+              <span className="mx-6 md:mx-10">MongoDB</span>
+              <span className="mx-6 md:mx-10">MySQL</span>
+              <span className="mx-6 md:mx-10">NumPy</span>
+              <span className="mx-6 md:mx-10">Pandas</span>
+              <span className="mx-6 md:mx-10">TensorFlow</span>
+              <span className="mx-6 md:mx-10">Keras</span>
             </div>
             <div className={cn(styles['move-left-to-right'], "text-xl whitespace-nowrap ")} style={{ animationDuration: `${getRandomSpeed()}s` }}>
-              <span className="mx-6">React</span>
-              <span className="mx-6">Next.js</span>
-              <span className="mx-6">Django</span>
-              <span className="mx-6">Flask</span>
-              <span className="mx-6">Ruby on Rails</span>
-              <span className="mx-6">Node.js</span>
+              <span className="mx-6 md:mx-10">React</span>
+              <span className="mx-6 md:mx-10">Next.js</span>
+              <span className="mx-6 md:mx-10">Django</span>
+              <span className="mx-6 md:mx-10">Flask</span>
+              <span className="mx-6 md:mx-10">Ruby on Rails</span>
+              <span className="mx-6 md:mx-10">Node.js</span>
+              <span className="mx-6 md:mx-10">React</span>
+              <span className="mx-6 md:mx-10">Next.js</span>
+              <span className="mx-6 md:mx-10">Django</span>
+              <span className="mx-6 md:mx-10">Flask</span>
+              <span className="mx-6 md:mx-10">Ruby on Rails</span>
+              <span className="mx-6 md:mx-10">Node.js</span>
             </div>
             <div className={cn(styles['move-right-to-left'], "text-xl whitespace-nowrap ")} style={{ animationDuration: `${getRandomSpeed()}s` }}>
-              <span className="mx-6">Python</span>
-              <span className="mx-6">JavaScript</span>
-              <span className="mx-6">C</span>
-              <span className="mx-6">C++</span>
-              <span className="mx-6">C#</span>
-              <span className="mx-6">SQL</span>
+              <span className="mx-6 md:mx-10">Python</span>
+              <span className="mx-6 md:mx-10">JavaScript</span>
+              <span className="mx-6 md:mx-10">C</span>
+              <span className="mx-6 md:mx-10">C++</span>
+              <span className="mx-6 md:mx-10">C#</span>
+              <span className="mx-6 md:mx-10">SQL</span>
+              <span className="mx-6 md:mx-10">Python</span>
+              <span className="mx-6 md:mx-10">JavaScript</span>
+              <span className="mx-6 md:mx-10">C</span>
+              <span className="mx-6 md:mx-10">C++</span>
+              <span className="mx-6 md:mx-10">C#</span>
+              <span className="mx-6 md:mx-10">SQL</span>
             </div>
           </div>
         </div>
